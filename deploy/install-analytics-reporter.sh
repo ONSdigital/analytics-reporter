99a3f117d2e9ab93d754b93e4befbec2e64c952c
Ian Kent <email@iankent.co.uk>
2016-01-08 11:41:47 +0000

    fix file path

--- deploy/install-analytics-reporter.sh
+++ deploy/install-analytics-reporter.sh
@@ -7,7 +7,7 @@ ssh $EC2_USER@$EC2_HOST sudo apt-get install npm
 ssh $EC2_USER@$EC2_HOST sudo npm install -g analytics-reporter
 
 scp ../../ONS-Analytics-95eb94bbb46f.json $EC2_USER@$EC2_HOST:ONS-Analytics-95eb94bbb46f.json
-scp reports.json $EC2_USER@$EC2_HOST:reports.json
+scp ../reports/ons.json $EC2_USER@$EC2_HOST:reports.json
 scp daily.sh $EC2_USER@$EC2_HOST:daily.sh
 ssh $EC2_USER@$EC2_HOST chmod +x daily.sh
 scp realtime.sh $EC2_USER@$EC2_HOST:realtime.sh
